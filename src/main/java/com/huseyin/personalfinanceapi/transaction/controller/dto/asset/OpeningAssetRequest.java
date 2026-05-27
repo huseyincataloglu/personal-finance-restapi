97ff6e7ad7fb07750ca7907dead0806b4bbabc68
hüseyin <73530226+huseyincataloglu@users.noreply.github.com>
2026-05-27 14:57:48 +0300

    Some files adjusted

---
+++ src/main/java/com/huseyin/personalfinanceapi/transaction/controller/dto/asset/OpeningAssetRequest.java
@@ -0,0 +1,25 @@
+package com.huseyin.personalfinanceapi.transaction.controller.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.List;
+
+
+public record OpeningAssetRequest(
+        @NotNull Long assetAccountId,
+        @NotEmpty List<AssetItemLine> assetItemLineList
+) {
+
+
+
+    public record AssetItemLine(
+            @NotNull @Positive Long assetId,   // Varlığın sistemdeki ID'si (Örn: THYAO, Altın)
+            @NotNull @Positive BigDecimal quantity,// Miktar (Örn: 10 adet veya 5.5 gram)
+            @NotNull @Positive BigDecimal unitPrice// Alış birim fiyatı (Maliyet hesabı için)
+    ) {}
+
+}
